8709e7931677d8e49e5595353a37c55640092488
Roflonzo <rolf.kairo@tktk.ee>
2023-01-20 05:13:46 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+using System.Runtime.Serialization.Formatters;
+
+Random rnd = new Random();
+
+
+int MyRandomNum;
+
+int randomSum = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    MyRandomNum = rnd.Next(0, 11); //0-10
+    randomSum = randomSum + MyRandomNum;
+    Console.WriteLine($"My random number is: {MyRandomNum}");
+}
+Console.WriteLine($"random sum total: {randomSum}");
